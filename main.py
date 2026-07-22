adebf581b48f904c6f54b845c99fdfe1e5f59e90
Aman1n <amanaggarwal2004@gmail.com>
2026-07-23 00:26:00 +0530

    abc1

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class User(BaseModel):
 @app.post("/create-user")
 def create_user(user: User):
     """
-    Creates a new user.
+    Creates a new user. with this api you can create a new user by providing the username and age in the request body. The endpoint will return a success message along with the created user details.
     """
     return {'MESSAGE': 'User created successfully', 'user': user}
 
