691afc2d541a63e72419449ff4b6970c58205a77
Aman1n <amanaggarwal2004@gmail.com>
2026-07-23 00:17:22 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class User(BaseModel):
+    username: str
+    age: int
+
+@app.post("/create-user")
+def create_user(user: User):
+    return {'MESSAGE': 'User created successfully', 'user': user}
+
