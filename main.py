6c864a28bdcdae2566db9352893539ca5fbfe8c9
Aman1n <amanaggarwal2004@gmail.com>
2026-07-24 15:35:39 +0530

    pydantic work

--- main.py
+++ main.py
@@ -3,11 +3,30 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+# class User(BaseModel):
+#     username: str
+#     age: int
+#     email: str
+
+# @app.post("/create-user")
+# def create_user(user: User):
+#     return {'MESSAGE': 'User created successfully',
+#              'user': user}
+
+class address(BaseModel):
+    street: str
+    city: str
+    state: str
+    pincode: int
+
 class User(BaseModel):
     username: str
     age: int
+    email: str
+    address: address
 
-@app.post("/create-user")
-def create_user(user: User):
-    return {'MESSAGE': 'User created successfully', 'user': user}
+@app.post("/create-user-with-address")
+def create_user_with_address(user: User):
+    return {'MESSAGE': 'User with address created successfully',
+             'user': user}
 
