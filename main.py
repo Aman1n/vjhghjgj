139b05c68cc88f97e4661e58be170b6d655decd9
Aman1n <amanaggarwal2004@gmail.com>
2026-08-06 14:39:41 +0530

    Update the list

--- main.py
+++ main.py
@@ -58,3 +58,14 @@ def get_todo(todo_id:int, db: Session = Depends(get_db)):
     if not todo:
         raise HTTPException(status_code=404, detail="Todo not found")
     return todo
+
+# Update
+@app.put("/todos/{todo_id}")
+def update_todo(todo_id:int, title:str, db: Session = Depends(get_db)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if not todo:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    todo.title = title
+    db.commit()
+    db.refresh(todo)
+    return todo
