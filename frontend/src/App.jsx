2677db1282d2b45930008cbd9a638db123eeaaa7
Aman1n <amanaggarwal2004@gmail.com>
2026-08-19 17:54:24 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/")
+      .then((response) => response.json())
+      .then(json => setData(json))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>API DATA</h1>
+      {data ? (
+        <pre>Message: {data.message}</pre>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  )
+}
+export default App;
